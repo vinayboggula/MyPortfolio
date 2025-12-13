9a127917ab3e5be1991b4da1a269322619c2dfc2
vinayboggula <boggulavinay26@gmail.com>
2025-12-13 22:34:50 +0530

    theme changed to dark as default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(true)
+    const [isDarkMode, setIsDarkMode] = useState(false)
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
         if (storedTheme === 'dark') {
